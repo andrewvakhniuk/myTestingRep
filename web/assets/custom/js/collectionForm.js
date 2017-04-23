41a0774fb4d1a188d7459afcb6486560e724c479
Vladyslav Lysenko <va_lysenko@ukr.net>
2017-04-23 14:57:15 +0300

    multiple collections

--- web/assets/custom/js/collectionForm.js
+++ web/assets/custom/js/collectionForm.js
@@ -1,6 +1,5 @@
-var $collectionHolder;
-var $addLinkHolder;
-var $deleteLink;
+var $collectionHolder = [];
+var $deleteLink = [];
 
 /**
  *
@@ -8,32 +7,34 @@ var $deleteLink;
  * @param addLink
  * @param deleteLink
  */
-function initCollection(collectionHolder, addLink, deleteLink) {
-    $collectionHolder = $(collectionHolder);
-    $deleteLink  = deleteLink;
+function initCollection(collectionHolder, addLink, deleteLink, indexNumber) {
+    $collectionHolder[indexNumber] = $(collectionHolder);
+    $deleteLink[indexNumber]  = deleteLink;
 
     // setup an "add" link
     var $addLink = $(addLink);
 
     // add a delete link to all of the existing elements
-    $collectionHolder.find('.collection').each(function() {
-        addTagFormDeleteLink($(this));
+    $collectionHolder[indexNumber].find('.collection' + indexNumber).each(function() {
+        addTagFormDeleteLink($(this), indexNumber);
     });
-    $collectionHolder;
 
-
-    $collectionHolder.append($addLink);
+    $collectionHolder[indexNumber].append($addLink);
 
     // count the current form inputs we have (e.g. 2), use that as the new
     // index when inserting a new item (e.g. 2)
-    $collectionHolder.data('index', $collectionHolder.find(':input').length);
+
+    // var numberOfInputs = $collectionHolder[indexNumber].find(':input').length;
+    var numberOfElements = $collectionHolder[indexNumber].find('.collection' + indexNumber).length;
+    $collectionHolder[indexNumber].data('index', numberOfElements);
+
 
     $addLink.on('click', function(e) {
         // prevent the link from creating a "#" on the URL
         e.preventDefault();
 
         // add a new form
-        addTagForm($collectionHolder, $addLink);
+        addTagForm($collectionHolder[indexNumber], $addLink, indexNumber);
     });
 
 }
@@ -43,7 +44,8 @@ function initCollection(collectionHolder, addLink, deleteLink) {
  * @param $collectionHolder
  * @param $addLink
  */
-function addTagForm($collectionHolder, $addLink) {
+function addTagForm($collectionHolder, $addLink, indexNumber) {
+
     // Get the data-prototype explained earlier
     var prototype = $collectionHolder.data('prototype');
 
@@ -52,31 +54,37 @@ function addTagForm($collectionHolder, $addLink) {
 
     // Replace '__name__' in the prototype's HTML to
     // instead be a number based on how many items we have
-    var newForm = prototype.replace(/__name__/g, index);
+    // var newForm = prototype.replace(/__name__/g, index);
+
+
+    if($collectionHolder.attr('id') == 'fieldTypes') {
+        var newForm = prototype.replace(/__fieldTypes__/g, index);
+    } else if($collectionHolder.attr('id') == 'choiceItems' + indexNumber) {
+        var newForm = prototype.replace(/__choiceItems__/g, index);
+    }
 
     // increase the index with one for the next item
     $collectionHolder.data('index', index + 1);
 
-    var $newFormLi = $('<li class="collection"></li>').append(newForm);
+    var $newFormLi = $('<li class="collection'+ indexNumber + '"></li>').append(newForm);
 
     $addLink.before($newFormLi);
    // $('select').material_select();
 
-    var $lastCollectionChild = $('.collection').last();
+    var $lastCollectionChild = $('.collection' + indexNumber).last();
 
     // add a delete link to the new form
-    addTagFormDeleteLink($lastCollectionChild);
+    addTagFormDeleteLink($lastCollectionChild, indexNumber);
 }
 
 /**
  *
  * @param $form
  */
-function addTagFormDeleteLink($form) {
-    var $removeFormA = $($deleteLink);
+function addTagFormDeleteLink($form, indexNumber) {
+    var $removeFormA = $($deleteLink[indexNumber]);
 
     $form.append($removeFormA);
-
 
     $removeFormA.on('click', function(e) {
         // prevent the link from creating a "#" on the URL
